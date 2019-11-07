7b932dafc6fb91d641ffe3dd6da275615cd66995
YaCpotato <yasshisshy@gmail.com>
2019-11-07 18:13:58 +0900

    404 API実装に映る

--- databese/index.js
+++
@@ -1,8 +0,0 @@
-import { Database } from '@vuex-orm/core'
-import ToDo from '@/models/ToDo'
-
-const database = new Database()
-
-database.register(ToDo)
-
-export default database
--- store/index.js
+++ store/index.js
@@ -1,26 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Veux from 'vuex'
 import VuexORM from '@vuex-orm/core'
 import VuexORMAxios from '@vuex-orm/plugin-axios'
-import database from './database'
-import axios from 'axios'
-Vue.use(Vuex)
+import ToDo from '@/models/ToDo'
+
+VuexORM.use(VuexORMAxios) // <- No axios option.
+
+const database = new VuexORM.Database()
+
+database.register(ToDo)
+
+export const plugins = [VuexORM.install(database)]
+
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+// import VuexORM from '@vuex-orm/core'
+// import VuexORMAxios from '@vuex-orm/plugin-axios'
+// import database from './database'
+// import axios from 'axios'
+// Vue.use(Vuex)
 
 
-// console.log(new Vue())
+// // console.log(new Vue())
 
-VuexORM.use(VuexORMAxios, {
-  axios,
-  http: {
-    baseURL: 'http://localhost:28017/project_phaser/project',
-    url: '/',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }
-})
+// VuexORM.use(VuexORMAxios, {
+//   axios,
+//   http: {
+//     baseURL: 'http://localhost:28017/project_phaser/project',
+//     url: '/',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json'
+//     }
+//   }
+// })
 
-export default () => new Vuex.Store({
-  plugins: [VuexORM.install(database)]
-})
+// export default () => new Vuex.Store({
+//   plugins: [VuexORM.install(database)]
+// })
